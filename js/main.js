fc2bee5df1ebfdd76140e6ff18bd935cc1e53245
projectnum17 <105784031+projectnum17@users.noreply.github.com>
2022-05-25 17:55:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const hamb = document.querySelector('.hamb');
+const popup = document.querySelector ('.popup');
+const menu = document.querySelector('.menu').cloneNode(1)
+const body = document.body
+
+hamb.addEventListener('click', hambHandler);
+
+function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll")
+    renderPopUp();
+} 
+
+function renderPopUp() {
+    popup.appendChild(menu);
+}
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
